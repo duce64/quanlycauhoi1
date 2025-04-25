480a00c504a9335232b22ac79a23226ba1978a83
duce64 <nguyenduc250202@gmail.com>
2025-04-25 14:34:18 +0700

    ds
    sd

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "625194894"
+    serviceWorkerVersion: "1573940369"
   }
 });
