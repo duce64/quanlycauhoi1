38c9997dbd0fa0559086b797f4415306224b3996
duce64 <nguyenduc250202@gmail.com>
2025-04-28 14:16:09 +0700

    a
    add

--- lib/configdomain.dart
+++ lib/configdomain.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String baseUrl = 'http://192.168.52.91:3000';
+  static const String baseUrl = 'https://nodejs-u3zp.onrender.com';
   //http://192.168.52.91:3000/
   //https://nodejs-u3zp.onrender.com
 }
